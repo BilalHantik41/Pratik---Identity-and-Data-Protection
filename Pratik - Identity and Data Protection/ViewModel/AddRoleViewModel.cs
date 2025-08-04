615335bd36bcf75d0e94d0bc07c3704080c3aa12
Bilal Hantik <bilalhantik@gmail.com>
2025-08-04 22:07:52 +0300

    Add project files.

---
+++ Pratik - Identity and Data Protection/ViewModel/AddRoleViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Pratik___Identity_and_Data_Protection.ViewModel
+{
+    public class AddRoleViewModel
+    {
+        public string UserId { get; set; }
+        public string RoleName { get; set; }
+
+    }
+}
